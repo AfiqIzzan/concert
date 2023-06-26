bff80af5072b1eb21cd0868f9848fdd3b9501fdf
AfiqIzzan <afiqizzan16@gmail.com>
2023-06-26 12:47:36 +0800

    devise user

---
+++ config/routes/user.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+    devise_for :users, :controllers => {:registrations => "users/registrations", :sessions => "users/sessions"}
+
+    devise_scope :user do
+        authenticated :user do
+            root "users#homeUser",  as: :authenticated_user_root
+        end
+        unauthenticated :user do
+            root 'users#testUser', as: :unauthenticated_user_root
+        end
+        # get 'users/login', to: 'users/sessions#new'
+        # get 'users/signup', to: 'users/registrations#new'
+        # get 'users/sign_out', to: 'sessions#destroy'
+    end
+end
